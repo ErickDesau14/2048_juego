cb93dfe07e905418c62a7d47e03b8170afa841ca
Erick de la Cruz <erick@kohmi.net>
2024-10-15 11:49:02 -0600

    Evento swipe con GestController

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, viewChild } from '@angular/core';
 import { Cell } from '../models/cell';
+import { GestureController, GestureDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.page.html',
   styleUrls: ['./game.page.scss'],
 })
-export class GamePage implements OnInit {
+export class GamePage implements AfterViewInit {
+
+  @ViewChild('boardGame', {read: ElementRef})
+  boardGame: ElementRef;
 
   public board: Cell[][]; // Matriz de celdas
 
   public rows: number[];
   public cols: number[];
 
-  constructor() { 
+  constructor(
+    private gestureController: GestureController
+  ) { 
 
     this.board = [
       [null, null, null, null], 
@@ -26,8 +32,37 @@ export class GamePage implements OnInit {
 
 
   }
+  ngAfterViewInit(): void {
 
-  ngOnInit() {
+    const hSwipe = this.gestureController.create({
+      el: this.boardGame.nativeElement,
+      gestureName: 'hswipe',
+      maxAngle: 30,
+      direction: 'x',
+      onEnd: (detail) => this.onHSwipe(detail)
+    }, true)
+
+    const vSwipe = this.gestureController.create({
+      el: this.boardGame.nativeElement,
+      gestureName: 'vswipe',
+      maxAngle: 30,
+      direction: 'y',
+      onEnd: (detail) => this.onVSwipe(detail)
+    }, true)
+
+    vSwipe.enable();
+    hSwipe.enable();
+
+  }
+
+  onHSwipe(detail: GestureDetail){
+    console.log("Horizontal");
+    console.log(detail);    
+  }
+
+  onVSwipe(detail: GestureDetail){
+    console.log("Vertical");
+    console.log(detail);    
   }
 
 }
