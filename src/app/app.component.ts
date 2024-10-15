f052b8540d49d601f7b8106e193d0f006022dd00
Erick de la Cruz <erick@kohmi.net>
2024-10-15 10:39:03 -0600

    Seteando traducciones en app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Device } from '@capacitor/device';
+import { Platform } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private translateService: TranslateService
+  ) {
+    this.translateService.setDefaultLang('es');
+    this.initApp();
+  }
+
+  initApp() {
+
+    this.platform.ready().then( async () => {
+      const language = await Device.getLanguageCode();
+
+      if(language.value){
+        this.translateService.use(language.value.slice(0, 2));
+      }
+
+    })
+
+  }
+
 }
